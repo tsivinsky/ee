ebd594432e2d4dc9d26f33a383b3f0df823780b9
Daniil Tsivinsky <daniil@tsivinsky.com>
2023-03-18 18:03:29 +0300

    feat!: store handlers as structs with id and func

--- ee.go
+++ ee.go
@@ -5,7 +5,12 @@ import (
 	"fmt"
 )
 
-type EventHandler = func(data ...any)
+type EventHandlerFunc = func(data ...any)
+
+type EventHandler struct {
+	Id   int
+	Func EventHandlerFunc
+}
 
 type EventEmitter struct {
 	events map[string][]EventHandler
@@ -13,15 +18,19 @@ type EventEmitter struct {
 
 // On subscribes handler to event and returns index of handler
 // It also creates internal events map if struct wasn't initialized
-func (emitter *EventEmitter) On(event string, handler EventHandler) int {
+func (emitter *EventEmitter) On(event string, handlerFunc EventHandlerFunc) int {
 	if emitter.events == nil {
 		emitter.events = make(map[string][]EventHandler)
 	}
 
-	emitter.events[event] = append(emitter.events[event], handler)
-	index := len(emitter.events[event]) - 1
+	handler := EventHandler{
+		Id:   len(emitter.events[event]) - 1,
+		Func: handlerFunc,
+	}
 
-	return index
+	emitter.events[event] = append(emitter.events[event], handler)
+
+	return handler.Id
 }
 
 // Emit calls all handlers subscribed to event and returns error if event doesn't exist in map
@@ -32,7 +41,7 @@ func (emitter *EventEmitter) Emit(event string, data ...any) error {
 	}
 
 	for _, handler := range handlers {
-		handler(data...)
+		handler.Func(data...)
 	}
 
 	return nil
@@ -50,12 +59,17 @@ func (emitter *EventEmitter) Remove(event string) error {
 }
 
 // Off unsubscribes handler from event and returns error if event doesn't exist
-func (emitter *EventEmitter) Off(event string, index int) error {
+func (emitter *EventEmitter) Off(event string, handlerId int) error {
 	if _, ok := emitter.events[event]; !ok {
 		return errors.New(fmt.Sprintf("\"%s\" event does not exist. Register it with ee.On(\"%s\", handler)", event, event))
 	}
 
-	emitter.events[event] = append(emitter.events[event][:index], emitter.events[event][index+1:]...)
+	for i, handler := range emitter.events[event] {
+		if handler.Id == handlerId {
+			emitter.events[event] = append(emitter.events[event][:i], emitter.events[event][i+1:]...)
+			break
+		}
+	}
 
 	return nil
 }
